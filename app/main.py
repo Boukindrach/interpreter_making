9d230fdc509580823dadfcd49aae67dd8f959b7f
Boukind Rachid <boukind.r1@gmail.com>
2025-05-09 00:26:06 +0100

    handling ==

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def main():
         i = 0
         while i < len(file_contents):
             if file_contents[i] in valid_tokens:
-                if file_contents[i] == "=" and file_contents[i + 1] == '=' and i + 1 < len(file_contents):
+                if file_contents[i] == "=" and i + 1 < len(file_contents) and file_contents[i + 1] == '=':
                     print(f"EQUAL_EQUAL == null")
                     i += 2
                 else:

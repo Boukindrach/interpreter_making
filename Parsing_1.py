c35acad26dc428d3927751e669d5cea595af1bc7
Rachid Boukind <boukind.r1@gmail.com>
2025-05-13 23:21:46 +0100

    prasing expressions in the making

--- Parsing_1.py
+++ Parsing_1.py
@@ -5,15 +5,24 @@ def parse(file_contects):
         i = 0
         num_str = ""
         while i < len(file_contects):
-            num_str = ""
-            while i < len(file_contects) and file_contects[i].isdigit():
-                num_str += file_contects[i]
-                i += 1
-                if file_contects[i] == ' ':
-                    num_list.append(num_str + '.0')
+            if 'a' <= file_contects[i] <= 'z':
+                a_str = ""
+                while i < len(file_contects) and 'a' <= file_contects[i] <= 'z':
+                    a_str += file_contects[i]
+                    if file_contects[i] == " ":
+                        break
                     i += 1
-                    break
-            while i < len(file_contects) and not file_contects[i].isdigit():
-                other.append(file_contects[i])
-                i += 1
-        print("(" + ''.join(other) + ' '.join(num_list) + ")")
+                print(a_str)
+            elif file_contects[i].isdigit():
+                num_str = ""
+                while i < len(file_contects) and file_contects[i].isdigit():
+                    num_str += file_contects[i]
+                    i += 1
+                    if file_contects[i] == ' ':
+                        num_list.append(num_str + '.0')
+                        i += 1
+                        break
+                while i < len(file_contects) and not file_contects[i].isdigit():
+                    other.append(file_contects[i])
+                    i += 1
+                print("(" + ''.join(other) + ' '.join(num_list) + ")")

a0e3af063879588a538d0aaa0e6ddf5f02e80ec7
Rachid Boukind <boukind.r1@gmail.com>
2025-05-14 17:23:52 +0100

    now supporting for parsing number literals

--- Parsing_1.py
+++ Parsing_1.py
@@ -1,9 +1,9 @@
 def parse(file_contects):
     if file_contects:
+        i = 0
         num_list = []
         other = []
-        i = 0
-        num_str = ""
+        str_ = ''
         while i < len(file_contects):
             if 'a' <= file_contects[i] <= 'z':
                 a_str = ""
@@ -13,16 +13,28 @@ def parse(file_contects):
                         break
                     i += 1
                 print(a_str)
+                exit(1)
             elif file_contects[i].isdigit():
-                num_str = ""
-                while i < len(file_contects) and file_contects[i].isdigit():
-                    num_str += file_contects[i]
+                num = []
+                while (i < len(file_contects)) and (file_contects[i].isdigit() or file_contects[i] == '.'):
+                    num.append(file_contects[i])
                     i += 1
-                    if file_contects[i] == ' ':
-                        num_list.append(num_str + '.0')
-                        i += 1
-                        break
-                while i < len(file_contects) and not file_contects[i].isdigit():
-                    other.append(file_contects[i])
+                if '.' not in num:
+                    num += ".0"
+                    num_list.append(''.join(num))
+                else:
+                    num_list.append(''.join(num))
+            elif not file_contects[i].isspace():
+                str_ = []
+                while i < len(file_contects) and not file_contects[i].isspace() and not file_contects[i].isdigit():
+                    str_.append(file_contects[i])
                     i += 1
-                print("(" + ''.join(other) + ' '.join(num_list) + ")")
+                other.append(''.join(str_))
+            else:
+                i += 1
+        if other and num_list:
+            print("("+ ''.join(other) + ' ' + ' '.join(num_list) + ")")
+        elif other:
+            print("("+ ''.join(other) + ")")
+        elif num_list:
+            print(' '.join(num_list))

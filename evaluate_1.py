427aa11e5ba4cc81e5e5fae34522833df407f566
Rachid Boukind <boukind.r1@gmail.com>
2025-05-24 11:23:37 +0100

    evaluate

--- evaluate_1.py
+++ evaluate_1.py
@@ -2,7 +2,7 @@ def evaluate(file_contents):
     t = ["true", "false", "nil"]
     s = ['+', '-']
     str = ''
-    i = 0
+    long_string = file_contents
     for token in file_contents.split():
         if token[0] >= '0' and token[0] <= '9':
             if '.' in token:
@@ -10,7 +10,13 @@ def evaluate(file_contents):
             else:
                 print(token)
         else:
-            if token[0] == '"' and token[-1] == '"':
-                print(token[1: -1])
+            if token[0] == '"':
+                q = 1
+                for i in long_string[1:]:
+                    str += i
+                    if i == '"':
+                        print(str[:-1])
+                        return
+
             else:
                 print(token)
